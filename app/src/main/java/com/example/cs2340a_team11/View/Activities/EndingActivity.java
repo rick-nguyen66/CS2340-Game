45c0be33c7e49c93713377228bdf6562472910a4
Rick H Nguyen <rick248nguyen@gmail.com>
2024-02-05 01:53:08 -0500

    Add signup screen with authentication

--- app/src/main/java/com/example/cs2340a_team11/View/Activities/EndingActivity.java
+++ app/src/main/java/com/example/cs2340a_team11/View/Activities/EndingActivity.java
@@ -1,21 +1,34 @@
 package com.example.cs2340a_team11.View.Activities;
 
+import static androidx.constraintlayout.widget.StateSet.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cs2340a_team11.Model.Player;
 import com.example.cs2340a_team11.R;
 import com.example.cs2340a_team11.ViewModel.EndScreenViewModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class EndingActivity extends AppCompatActivity {
-
     private EndScreenViewModel endScreenViewModel;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Player player = Player.getPlayer();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ending_screen);
@@ -26,7 +39,25 @@ public class EndingActivity extends AppCompatActivity {
         currentScore.setText("Last attempted score: "
                 + Integer.toString(endScreenViewModel.calcTotalScore()));
 
-        // add score to database
+        Map<String, Object> user = new HashMap<>();
+        user.put("player", player.getName());
+        user.put("score", player.getScore());
+
+        // add score to database (generally use a hashmap for multiple data
+        db.collection("scores")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
 
         TextView scoreList = findViewById(R.id.scoreList);
         scoreList.setText(endScreenViewModel.getScores());
@@ -41,7 +72,7 @@ public class EndingActivity extends AppCompatActivity {
     }
     public void restartGame() {
         endScreenViewModel.resetScore();
-        Intent restart = new Intent(this, StartScreen.class);
+        Intent restart = new Intent(this, InitialConfigScreen.class);
         startActivity(restart);
     }
 }
--- app/src/main/java/com/example/cs2340a_team11/View/Activities/StartScreen.java
+++ app/src/main/java/com/example/cs2340a_team11/View/Activities/StartScreen.java
@@ -1,17 +1,27 @@
 package com.example.cs2340a_team11.View.Activities;
 
+import static androidx.constraintlayout.widget.StateSet.TAG;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.cs2340a_team11.R;
-import com.example.cs2340a_team11.View.Activities.InitialConfigScreen;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class StartScreen extends Activity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +29,63 @@ public class StartScreen extends Activity {
         //create activity_start_screen XML file
 
         TextView welcomeText = findViewById(R.id.welcomeText);
-        Button startButton = findViewById(R.id.startButton);
+        // Button startButton = findViewById(R.id.signInButton);
         Button quitButton = findViewById(R.id.quitButton);
+
+        TextView email = findViewById(R.id.emailField);
+        TextView password = findViewById(R.id.passwordField);
+        Button signInButton = findViewById(R.id.signInButton);
+        Button createAcc = findViewById(R.id.createAccount);
 
         // Set text to something better if wanted
         welcomeText.setText("Welcome!");
 
+        mAuth = FirebaseAuth.getInstance();
+
+        createAcc.setOnClickListener(view -> {
+            mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // if signin successful, update UI with user info
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                // updateUI(user);
+                                startGame();
+                            } else {
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(StartScreen.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                // updateUI(null);
+                            }
+                        }
+                    });
+        });
+
+        signInButton.setOnClickListener(view -> {
+            mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                // updateUI(user);
+
+                                startGame();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(StartScreen.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                // updateUI(null);
+                            }
+                        }
+                    });
+        });
+        /*
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,16 +95,31 @@ public class StartScreen extends Activity {
                 //endButton.setVisibility(View.VISIBLE);
             }
         });
+         */
 
         quitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                welcomeText.setVisibility(View.GONE);
-                startButton.setVisibility(View.GONE);
+                // welcomeText.setVisibility(View.GONE);
+                // startButton.setVisibility(View.GONE);
                 finish();
             }
         });
     }
+
+    /* If we want the user to auto sign-in, then include this
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            reload(); // DO: check if user signed-in (not null), update UI accordingly
+        }
+    }
+
+    private void reload() { }
+     */
+
 
     private void startGame() {
         Intent initialConfigIntent = new Intent(this, InitialConfigScreen.class);
